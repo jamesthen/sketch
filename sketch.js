ba6d255c9e93d94e636852b9c77968936f8ebf65
jamesthen <18248196+jamesthen@users.noreply.github.com>
2024-08-31 09:18:08 -0400

    Added reset button

--- sketch.js
+++ sketch.js
@@ -7,7 +7,13 @@ const drawBoard = () => {
     for (let i = 0; i < gridSize; i++) {
         const div = document.createElement('div');
         div.addEventListener('mouseover', ()=> {
-            div.classList.add('hover')
+            div.classList.add('hover');
+        })
+        div.addEventListener('click', ()=> {
+            div.classList.add('click');
+        })
+        div.addEventListener('dblclick', ()=> {
+            div.classList.remove('click', 'hover');
         })
         div.classList.add('cell');
         container.appendChild(div);
